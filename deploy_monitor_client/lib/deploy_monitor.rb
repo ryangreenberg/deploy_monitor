c77e76c9a86ea6533c142531bca165aead407b5a
Ryan Greenberg <ryangreenberg@gmail.com>
2012-08-03 09:27:23 -0700

    Provide access to steps for system

--- deploy_monitor_client/lib/deploy_monitor.rb
+++ deploy_monitor_client/lib/deploy_monitor.rb
@@ -6,5 +6,6 @@ module DeployMonitor; end
 require 'deploy_monitor/version'
 require 'deploy_monitor/client'
 require 'deploy_monitor/system'
+require 'deploy_monitor/step'
 require 'deploy_monitor/deploy'
 require 'deploy_monitor/exception_silencing_proxy'
--- deploy_monitor_client/lib/deploy_monitor/system.rb
+++ deploy_monitor_client/lib/deploy_monitor/system.rb
@@ -1,6 +1,6 @@
 module DeployMonitor
   class System
-    attr_accessor :deploy_monitor, :system_id, :name
+    attr_accessor :deploy_monitor, :system_id, :name, :steps
 
     def self.from_api(deploy_monitor, api_obj)
       system = self.new
@@ -12,6 +12,7 @@ module DeployMonitor
     def update_from_api(api_obj)
       self.system_id = api_obj['id']
       self.name = api_obj['name']
+      self.steps = api_obj['steps'].map {|ea| Step.from_api(deploy_monitor, ea)}
     end
   end
 end
