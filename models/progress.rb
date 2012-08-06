8332b65879022c734fb18fd32e13328ac6f03227
Ryan Greenberg <ryangreenberg@gmail.com>
2012-08-05 23:29:22 -0700

    Add duration to Progress

--- models/progress.rb
+++ models/progress.rb
@@ -7,6 +7,14 @@ class Progress < Sequel::Model
     :update => :updated_at,
     :update_on_create => true
 
+  def duration
+    if started_at && finished_at
+      finished_at - started_at
+    else
+      nil
+    end
+  end
+
   def to_hash(options = {})
     values
   end
