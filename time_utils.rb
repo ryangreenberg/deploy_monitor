ed0c9320c92a51e0de6639a4a9e2c4fa886131f0
Ryan Greenberg <ryangreenberg@gmail.com>
2012-08-05 23:02:27 -0700

    Add support for %?H format string

--- time_utils.rb
+++ time_utils.rb
@@ -11,9 +11,16 @@ module TimeUtils
   end
 
   # Replaces %H with hours, %M with minutes, %S with seconds
+  # Replaces (%?H) with hours if > 0
   def format_hms(format_str, total_secs)
     h, m, s = hms(total_secs)
     m, s = zero_pad(m, 2), zero_pad(s, 2)
-    format_str.gsub('%H', h.to_s).gsub('%M', m.to_s).gsub('%S', s.to_s)
+    str = format_str.gsub('%H', h.to_s).gsub('%M', m.to_s).gsub('%S', s.to_s)
+    if h > 0
+      str.gsub!(/\(%\?H(.*?)\)/, "#{h}\\1")
+    else
+      str.gsub!(/\(%\?H(.*?)\)/, h > 0 ? "#{h}\\1" : '')
+    end
+    str
   end
 end
