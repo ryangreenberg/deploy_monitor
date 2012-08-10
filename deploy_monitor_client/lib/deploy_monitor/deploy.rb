f69ccc93d4b6873eaf606d0dfd1f62cdd1b8f879
Ryan Greenberg <ryangreenberg@gmail.com>
2012-08-09 19:53:38 -0700

    Send failed, not failure

--- deploy_monitor_client/lib/deploy_monitor/deploy.rb
+++ deploy_monitor_client/lib/deploy_monitor/deploy.rb
@@ -38,7 +38,7 @@ module DeployMonitor
 
     def fail
       begin
-        RestClient.post "#{deploy_monitor.base_url}/deploys/#{deploy_id}/complete", {:result => :failure}
+        RestClient.post "#{deploy_monitor.base_url}/deploys/#{deploy_id}/complete", {:result => :failed}
         true
       rescue RestClient::Exception
         false
@@ -47,7 +47,7 @@ module DeployMonitor
 
     def complete
       begin
-        RestClient.post "#{deploy_monitor.base_url}/deploys/#{deploy_id}/complete", {:result => :failure}
+        RestClient.post "#{deploy_monitor.base_url}/deploys/#{deploy_id}/complete"
         true
       rescue RestClient::Exception
         false
