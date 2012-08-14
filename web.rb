46d56417161a9bade73437b737a9ec7217396301
Ryan Greenberg <ryangreenberg@gmail.com>
2012-08-13 23:05:46 -0700

    Add endpoint to return recent deploys

--- web.rb
+++ web.rb
@@ -28,4 +28,21 @@ class DeployMonitor::Web < Sinatra::Base
     @recent_deploys = Deploy.filter(:system => @system).order(:updated_at.desc).limit(10)
     erb :system
   end
+
+  get '/recent_deploys' do
+    updated_at = Time.at(params[:updated_at].to_i)
+    deploys = Deploy.filter(:active => false).
+      where {|o| o.finished_at >= updated_at }.
+      order(:finished_at.desc).
+      limit(10)
+
+    deploy_htmls = deploys.map do |deploy|
+      partial( :deploy_row, :locals => { :include_system => true, :deploy => deploy} )
+    end
+
+    {
+      :updated_at => Time.now.to_i,
+      :deploy_html => deploy_htmls.join('')
+    }.to_json
+  end
 end
