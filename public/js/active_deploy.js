fa79a353104bbadb3f80e518a4926fb0b3029160
Ryan Greenberg <ryangreenberg@gmail.com>
2012-08-31 00:35:45 -0700

    Add JS to update deploy info page

---
+++ public/js/active_deploy.js
@@ -0,0 +1,69 @@
+function ActiveDeploy ($node) {
+  this.$node = $node;
+  this.intervalDuration = 5000;
+}
+
+ActiveDeploy.prototype.startPeriodicUpdates = function() {
+  this.periodicUpdatesActive = true;
+  this.scheduleNextUpdate();
+};
+
+ActiveDeploy.prototype.stopPeriodicUpdates = function() {
+  this.periodicUpdatesActive = false;
+  clearTimeout(this.updateTimeout);
+};
+
+ActiveDeploy.prototype.scheduleNextUpdate = function() {
+  if (this.periodicUpdatesActive) {
+    this.updateTimeout = setTimeout(this.update.bind(this), this.intervalDuration);
+  }
+};
+
+ActiveDeploy.prototype.update = function() {
+  var lastUpdatedAt = this.getUpdatedAt();
+  var deployId = this.getDeployId();
+
+  var request = $.ajax({
+    dataType: 'json',
+    data: {updated_at: lastUpdatedAt, id: deployId},
+    url: '/active_deploy'
+  });
+
+  request.done(this.display.bind(this));
+  request.complete(this.scheduleNextUpdate.bind(this));
+};
+
+ActiveDeploy.prototype.display = function(rsp) {
+  this.setUpdatedAt(rsp.updated_at);
+
+  if (rsp.progress_percentage) {
+    this.$node.find('.deploy-progress .bar').css('width', rsp.progress_percentage + "%");
+  }
+
+  if (rsp.current_progress) {
+    this.$node.find('.current-progress').text(rsp.current_progress);
+  }
+
+  if (rsp.deploy_steps) {
+    var deployStepsHtml = rsp.deploy_steps.map(function(step){ return step.html; }).join("");
+    this.$node.find('.steps').html(deployStepsHtml);
+  }
+
+  if (rsp.deploy_overview) {
+    this.$node.find('.deploy-overview').html(rsp.deploy_overview);
+    this.stopPeriodicUpdates();
+  }
+};
+
+ActiveDeploy.prototype.getDeployId = function() {
+  return this.$node.attr('data-deploy-id');
+};
+
+
+ActiveDeploy.prototype.getUpdatedAt = function() {
+  return this.$node.attr('data-updated-at');
+};
+
+ActiveDeploy.prototype.setUpdatedAt = function(timestamp) {
+  return this.$node.attr('data-updated-at', timestamp);
+};
