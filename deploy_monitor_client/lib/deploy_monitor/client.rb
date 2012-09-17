3ed52cd3e3264c3071a968741f04a3d4f3783215
Ryan Greenberg <ryangreenberg@gmail.com>
2012-09-17 09:15:47 -0700

    Move methods to get deploys to system

--- deploy_monitor_client/lib/deploy_monitor/client.rb
+++ deploy_monitor_client/lib/deploy_monitor/client.rb
@@ -27,25 +27,6 @@ module DeployMonitor
       RestClient.post "#{base_url}/systems", {:name => name}
     end
 
-    def start_deploy(system)
-      begin
-        rsp = RestClient.post "#{base_url}/#{system}/deploys", {}
-        Deploy.from_api(self, JSON.parse(rsp.body))
-      rescue RestClient::BadRequest => e
-        raise e, e.response
-      end
-    end
-
-    def current_deploy(system)
-      rsp = RestClient.get "#{base_url}/#{system}/deploys", :params => {:active => true}
-      deploys = JSON.parse(rsp.body)['deploys']
-      if deploys.empty?
-        nil
-      else
-        Deploy.from_api(self, deploys.first)
-      end
-    end
-
     def get_deploy(id)
       begin
         rsp = RestClient.get "#{base_url}/deploys/#{id}"
--- deploy_monitor_client/lib/deploy_monitor/system.rb
+++ deploy_monitor_client/lib/deploy_monitor/system.rb
@@ -1,5 +1,7 @@
 module DeployMonitor
   class System
+    include DeployMonitor::ApiObject
+
     attr_accessor :client, :system_id, :name, :steps
 
     def self.from_api(client, api_obj)
@@ -13,6 +15,25 @@ module DeployMonitor
       self.system_id = api_obj['id']
       self.name = api_obj['name']
       self.steps = api_obj['steps'].map {|ea| Step.from_api(client, ea)}
+    end
+
+    def start_deploy
+      begin
+        rsp = RestClient.post("#{base_url}/#{name}/deploys", {})
+        Deploy.from_api(self.client, JSON.parse(rsp.body))
+      rescue RestClient::BadRequest => e
+        raise e, e.response
+      end
+    end
+
+    def current_deploy
+      rsp = RestClient.get("#{base_url}/#{name}/deploys", :params => {:active => true})
+      deploys = JSON.parse(rsp.body)['deploys']
+      if deploys.empty?
+        nil
+      else
+        Deploy.from_api(self.client, deploys.first)
+      end
     end
   end
 end
