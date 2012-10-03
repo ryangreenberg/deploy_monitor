d781974e90c8f18a9608b9f2bd2d0259f56e6dc5
Ryan Greenberg <ryangreenberg@gmail.com>
2012-10-03 08:58:04 -0700

    "start deploy" is better than "create new deploy"

--- errors.rb
+++ errors.rb
@@ -1,14 +1,14 @@
 module Errors
   TYPES = {
-    :deploy_in_progress => "Cannot create new deploy for '%s' because deploy id %s is in progress",
+    :deploy_in_progress => "Cannot start deploy for '%s' because deploy id %s is in progress",
     :deploy_not_active => "Deploy is not active",
     :duplicate_deploy_step => "Deploy is already at step '%s'",
     :duplicate_entity => "%s already exists with id %s",
     :not_found => "%s could not be found",
     :required_param_missing => "Missing required param '%s'",
-    :system_has_no_steps => "Cannot create new deploy for '%s' because there are no steps",
+    :system_has_no_steps => "Cannot start deploy for '%s' because there are no steps",
     :unknown_result => "Cannot complete deploy using unknown result '%s'",
-    :unknown_step => "Cannot add unknown step '%s' to deploy",
+    :unknown_step => "Cannot advance deploy to unknown step '%s'",
   }
 
   def self.format(type, *strfmt_args)
