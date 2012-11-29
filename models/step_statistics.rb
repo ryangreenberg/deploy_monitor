57e43df3a6732976bae8a126d4e37ca24d1241ec
Ryan Greenberg <ryangreenberg@gmail.com>
2012-11-28 22:36:22 -0800

    Refactor duplicate code to with_completed_progresses

--- models/step_statistics.rb
+++ models/step_statistics.rb
@@ -9,21 +9,13 @@ class StepStatistics
   end
 
   def completion_rate_for_step_id(step_id)
-    progresses_for_step = @progresses.select {|ea| ea.step_id == step_id}
-    if progresses_for_step.empty?
-      nil
-    else
-      completed = progresses_for_step.select {|ea| ea.complete? }
-      completed.count.to_f / progresses_for_step.count
+    with_completed_progresses(step_id) do |progresses, completed|
+      completed.count.to_f / progresses.count
     end
   end
 
   def mean_duration_for_step_id(step_id)
-    progresses_for_step = @progresses.select {|ea| ea.step_id == step_id }
-    if progresses_for_step.empty?
-      nil
-    else
-      completed = progresses_for_step.select {|ea| ea.complete? }
+    with_completed_progresses(step_id) do |progresses, completed|
       completed.inject(0.0) {|sum, ea| sum + ea.duration } / completed.count
     end
   end
@@ -38,8 +30,13 @@ class StepStatistics
 
   private
 
-  # TODO Refactor to use this method
-  def progresses_for_step(step_id)
-    @progresses.select {|ea| ea.step_id == step_id }
+  def with_completed_progresses(step_id)
+    progresses_for_step = @progresses.select {|ea| ea.step_id == step_id }
+    if progresses_for_step.empty?
+      nil
+    else
+      completed_progresses = progresses_for_step.select {|ea| ea.complete? }
+      yield(progresses_for_step, completed_progresses)
+    end
   end
 end
