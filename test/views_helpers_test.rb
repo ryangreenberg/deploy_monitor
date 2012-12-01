c940f57f592179c9c4826cdc4de94640c43dc7c0
Ryan Greenberg <ryangreenberg@gmail.com>
2012-12-01 11:03:36 -0800

    Add helper to distort display of step bar
    The intended distortion is to make each segment of the progress bar a given
    minimum value so it is visible. Since a progress bar has a fixed width, we
    have to decrease all the other elements of the bar by some amount in order
    to accommodate the increases to elements below the minimum.

--- test/views_helpers_test.rb
+++ test/views_helpers_test.rb
@@ -21,4 +21,33 @@ describe ViewsHelpers do
       assert_equal "12.3", @instance.format_percent(0.123, 1).to_s
     end
   end
+
+  describe "#with_minimum_value" do
+    it "adjusts all values in the collection to be at least the provided value" do
+      arr = [0, 0, 10]
+      min_val = 1
+      adjusted_arr = @instance.with_minimum_value(arr, min_val)
+      assert adjusted_arr.all? {|ea| ea >= min_val }
+    end
+
+    it "maintains the total sum of the values in the collection" do
+      arr = [0, 0, 10]
+      adjusted_arr = @instance.with_minimum_value(arr, 1)
+      initial_sum = arr.inject(:+)
+      adjusted_sum = adjusted_arr.inject(:+)
+      assert_equal initial_sum, adjusted_sum
+    end
+
+    it "raises an error if maintaining the total sum is impossible for the given minimum value" do
+      arr = [0, 0, 10]
+      assert_raises(ArgumentError) { @instance.with_minimum_value(arr, 5) }
+    end
+
+    it "does not adjust any elements below the minimum value" do
+      arr = [0, 1.1, 8.9]
+      min_val = 1
+      adjusted_arr = @instance.with_minimum_value(arr, min_val)
+      assert adjusted_arr.all? {|ea| ea >= min_val }
+    end
+  end
 end
--- views_helpers.rb
+++ views_helpers.rb
@@ -42,4 +42,30 @@ module ViewsHelpers
     rounded_val = (value * 10**(2 + digits)).round.to_f / 10**(digits)
     digits == 0 ? rounded_val.to_i : rounded_val
   end
+
+  # Returns a collection where all elements are at least the provided minimum
+  # value without changing the sum of the elements. This is useful if you want
+  # to exaggerate some elements of a progress bar without changing the
+  # total width
+  def with_minimum_value(arr, min_val)
+    initial_sum = arr.inject(:+)
+    if min_val * arr.count > initial_sum
+      raise ArgumentError, "Cannot maintain initial sum #{initial_sum} and set all elements to a minimum value #{min_val}"
+    end
+
+    below_min, above_min = arr.partition {|ea| ea < min_val}
+    total_increase = below_min.inject(0.0) {|accum, i| accum + (min_val - i)}
+    total_available = above_min.map {|ea| ea - min_val }.inject(:+)
+
+    arr_with_min_vals = arr.map do |ea|
+      if ea < min_val
+        min_val
+      else
+        proportion_of_sum_above_min_val = (ea - min_val).to_f / total_available
+        ea - proportion_of_sum_above_min_val * total_increase
+      end
+    end
+
+    arr_with_min_vals
+  end
 end
