fed42f43193a51f73e183f98a013c014387a501a
Ryan Greenberg <ryangreenberg@gmail.com>
2012-12-11 21:03:43 -0800

    Add completion bounds to the API

--- test/models/deploy_test.rb
+++ test/models/deploy_test.rb
@@ -22,6 +22,7 @@ describe Deploy do
           :finished_at => now
         }))
         stub(deploy).completion_eta { Time.now }
+        stub(deploy).completion_eta_bounds { [Time.now, Time.now] }
         hsh = deploy.to_hash
         assert_equal now_timestamp, hsh[:created_at]
         assert_equal now_timestamp, hsh[:updated_at]
@@ -33,6 +34,7 @@ describe Deploy do
         deploy = Deploy.new(@default_attrs)
         stub(deploy).completion_probability { 0.50 }
         stub(deploy).completion_eta { Time.now }
+        stub(deploy).completion_eta_bounds { [Time.now, Time.now] }
         assert 0.50, deploy.to_hash[:completion_probability]
       end
 
@@ -41,6 +43,7 @@ describe Deploy do
         deploy = Deploy.new(@default_attrs)
         stub(deploy).completion_probability { 0.50 }
         stub(deploy).completion_eta { now }
+        stub(deploy).completion_eta_bounds { [Time.now, Time.now] }
         assert now.to_i, deploy.to_hash[:predicted_finished_at]
       end
     end
